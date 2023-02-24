ef2b6d920d9837018170fa412bcdc25cc8d61ffb
guilhermee010 <augustogarcia.ambrosio@gmail.com>
2023-02-24 01:21:19 -0300

    criação-formulario-cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatBottomSheetModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
